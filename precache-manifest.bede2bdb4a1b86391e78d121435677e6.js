0ae5229d4d42f042ad8598a77227fbd9ad182dc7
kdilot <kdilot@gmail.com>
2019-08-28 12:52:27 -0700

    Updates

--- precache-manifest.bede2bdb4a1b86391e78d121435677e6.js
+++ precache-manifest.bede2bdb4a1b86391e78d121435677e6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad1aee15afaf03e046c184f297a4c67c",
+    "revision": "c5f84772d4064616637c7f5b5ff1d27b",
     "url": "/page/index.html"
   },
   {
-    "revision": "74459f4444622749c204",
-    "url": "/page/static/css/main.546372a2.chunk.css"
+    "revision": "5f54bab6a6ead6bddc31",
+    "url": "/page/static/css/main.c4911caf.chunk.css"
   },
   {
     "revision": "b07e193b922d9c2b540a",
     "url": "/page/static/js/2.12e5c12b.chunk.js"
   },
   {
-    "revision": "74459f4444622749c204",
-    "url": "/page/static/js/main.ecc9c9d3.chunk.js"
+    "revision": "5f54bab6a6ead6bddc31",
+    "url": "/page/static/js/main.f1ccb6d5.chunk.js"
   },
   {
     "revision": "52f0d7f9f0fc1aa12f89",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/page/precache-manifest.6948a591c7e21d7bb3753c0c3f165348.js"
+  "/page/precache-manifest.bede2bdb4a1b86391e78d121435677e6.js"
 );
 
 self.addEventListener('message', (event) => {

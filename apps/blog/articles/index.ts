4b23327f99921287a073c386d4eef2c5f7a22fff
kdilot <kdilotdev@gmail.com>
2022-11-11 08:05:19 +0900

    nx next

--- apps/blog/articles/index.ts
+++ apps/blog/articles/index.ts
@@ -3,10 +3,11 @@ import NxNextMdx from './nx-next-mdx.mdx'
 import ReactImageIssue from './react-image-issue.mdx'
 import ReactLoadableComponent from './react-loadable-component.mdx'
 import ReactIE11Issue from './react-ie11-issue.mdx'
+import ReactWYSIWYG from './react-wysiwyg.mdx'
 
 export const agendas = {
   'Nx에서 생성된 NextJS 프로젝트에서 mdx 사용': NxNextMdx,
-  'NextJS svg 적용': NextSvg,
+  'NextJS svg 파일을 적용하기 위한 설정': NextSvg,
 }
 
 export const legacies = {
@@ -14,6 +15,7 @@ export const legacies = {
   'React 업로드 된 이미지 사용시 발생하는 이슈': ReactImageIssue,
   'React CRA 프로젝트에서 eject 없이 code spliting 시도하기':
     ReactLoadableComponent,
+  'React에서 eject 없이 사용할 수 있는 WYSIWYG 에디터': ReactWYSIWYG,
 }
 
 const files = { ...agendas, ...legacies }

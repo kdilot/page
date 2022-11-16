5454cf7fdfa799a51a32e70244c0fec0414d08c9
kdilot <kdilotdev@gmail.com>
2022-11-17 08:16:53 +0900

    nx next

--- apps/blog/articles/index.ts
+++ apps/blog/articles/index.ts
@@ -7,11 +7,14 @@ import ReactWYSIWYG from './react-wysiwyg.mdx'
 import ReactQuery from './react-query.mdx'
 import ReduxToolkit from './redux-toolkit.mdx'
 import ReactPortal from './react-portal.mdx'
+import ReactStorybookUpdateIssue from './react-storybook-update-issue.mdx'
 
 export const agendas = {
   'React Query 샘플 테스트': ReactQuery,
   'Redux Toolkit 샘플 테스트': ReduxToolkit,
   'React Portal로 모달을 만들어보자': ReactPortal,
+  'React CRA 프로젝트 storybook 버전 업데이트시 이슈':
+    ReactStorybookUpdateIssue,
   'Nx에서 생성된 NextJS 프로젝트에서 mdx 사용': NxNextMdx,
   'NextJS svg 파일을 적용하기 위한 설정': NextSvg,
 }
